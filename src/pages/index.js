010fa4f812ac2e0c0c9b601dcfecfbfba35f4623
“Steven <steven.gong@iag.com.au>
2018-05-15 22:57:30 +1000

    can read md files from index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,36 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default ({data}) => {
+  return (
+    <div>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+    <div key={node.id}>
+      <h5>
+        {node.frontmatter.title}{" "}
+        <span color="#BBB">— {node.frontmatter.date}</span>
+      </h5>
+      <p>{node.excerpt}</p>
+    </div>
+  ))}
+    </div>
+  )
+}
+
+export const query = graphql`
+query IndexQuery{
+  allMarkdownRemark {
+    edges {
+      node {
+        frontmatter {
+          title
+          path
+          date
+          _PARENT
+          parent
+        }
+        excerpt
+      }
+    }
+  }
+  }
+`;
